53992d7f109e6f40d13327d093f14c747211068b
nathan.williams <nathanidris.williams@de.amadeus.com>
2018-12-04 12:55:52 +0100

    added template day and utils package

---
+++ Day x template/Part 1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/IdrisTheDragon/AdventOfCode2018/utils"
+)
+
+func main() {
+	lines := utils.GetLines("../myInput.txt")
+
+	fmt.Println(lines)
+}
+
+
---
+++ utils/utils.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"regexp"
+)
+
+
+//GetLines gets the lines from a file
+func GetLines(fileName string) []string {
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lines := []string{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return lines
+}
+
+//RegSplit A simple regex splitter
+func RegSplit(text string, delimeter string) []string {
+	reg := regexp.MustCompile(delimeter)
+	indexes := reg.FindAllStringIndex(text, -1)
+	laststart := 0
+	result := make([]string, len(indexes)+1)
+	for i, element := range indexes {
+		result[i] = text[laststart:element[0]]
+		laststart = element[1]
+	}
+	result[len(indexes)] = text[laststart:len(text)]
+	return result
+}
