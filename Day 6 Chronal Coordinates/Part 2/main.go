f787a012d62a0354df176a8fe4a5a87aa16cc1e9
nathan.williams <nathanidris.williams@de.amadeus.com>
2018-12-06 09:52:50 +0100

    remove folder day 6

--- Day 6 Chronal Coordinates/Part 2/main.go
+++ Day 6 Chronal Coordinates/Part 2/main.go
@@ -2,28 +2,29 @@ package main
 
 import (
 	"fmt"
-	"github.com/IdrisTheDragon/AdventOfCode2018/utils"
 	"log"
 	"strconv"
+
+	"github.com/IdrisTheDragon/AdventOfCode2018/utils"
 )
 
 func main() {
 	lines := utils.GetLines("../myInput.txt")
 
-	x := make([] int, len(lines))
-	y := make([] int, len(lines))
+	x := make([]int, len(lines))
+	y := make([]int, len(lines))
 
-	bigX, bigY := 0,0
+	bigX, bigY := 0, 0
 
-	for n,v := range lines {
-		split := utils.RegSplit(v,"[, ]+")
+	for n, v := range lines {
+		split := utils.RegSplit(v, "[, ]+")
 		i, err := strconv.Atoi(split[0])
 		if err != nil {
 			log.Fatal(err)
 		}
 		x[n] = i
 
-		if(bigX < i){
+		if bigX < i {
 			bigX = i
 		}
 
@@ -33,22 +34,22 @@ func main() {
 		}
 		y[n] = j
 
-		if(bigY < j){
+		if bigY < j {
 			bigY = j
 		}
 	}
 
-	grid := make([][]int,bigX+1)
+	grid := make([][]int, bigX+1)
 	for i := range grid {
-		grid[i] = make ([]int, bigY+1)
+		grid[i] = make([]int, bigY+1)
 	}
 	region := 0
 	for i := 0; i < bigX+1; i++ {
 		for j := 0; j < bigY+1; j++ {
-			s := sum(i,j,x,y)
+			s := sum(i, j, x, y)
 			//fmt.Println(c)
 			grid[i][j] = s
-			if(s < 10000){
+			if s < 10000 {
 				region++
 			}
 		}
@@ -57,17 +58,17 @@ func main() {
 	fmt.Println(region)
 }
 
-func sum(x,y int , x1 , y1 [] int) int {
+func sum(x, y int, x1, y1 []int) int {
 	distance := 0
-	for n,_ := range y1 {
-		man := manhattanDistance(x,y,x1[n],y1[n])
+	for n, _ := range y1 {
+		man := manhattanDistance(x, y, x1[n], y1[n])
 		//fmt.Println(x,y,":",x1[n],y1[n],"",man)
 		distance = distance + man
 	}
 	return distance
 }
 
-func manhattanDistance(x1,y1,x2,y2 int) int {
+func manhattanDistance(x1, y1, x2, y2 int) int {
 	return abs(x1-x2) + abs(y1-y2)
 }
 
@@ -77,4 +78,3 @@ func abs(n int) int {
 	}
 	return n
 }
-
