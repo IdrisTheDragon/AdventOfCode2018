79802feb539943d1774f34c88a8d249dd1bfef19
nathan.williams <nathanidris.williams@de.amadeus.com>
2018-12-17 17:38:24 +0100

    Day 15 still unfinished..

--- Day 15/Part 1/main.go
+++ Day 15/Part 1/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"os/exec"
-	"time"
-	"math"
 	"sort"
+	"time"
+
 	//"log"
 
 	"github.com/IdrisTheDragon/AdventOfCode2018/utils"
@@ -17,8 +18,8 @@ const STARTING_HITPOINTS int = 200
 const ATTACK int = 3
 
 func main() {
-	lines := utils.GetLines("../myInput.txt")
-	//lines := utils.GetLines("../test1.txt")
+	//lines := utils.GetLines("../myInput.txt")
+	lines := utils.GetLines("../test1.txt")
 
 	cave := World{}
 	units := make([]*Unit, 0)
@@ -28,9 +29,9 @@ func main() {
 		for x, cell := range line {
 			if cell == '#' || cell == '.' {
 				cave.SetTile(&Tile{
-					kind:cell,
-					x:x,
-					y:y,
+					kind: cell,
+					x:    x,
+					y:    y,
 				})
 			} else {
 				units = append(units, &Unit{
@@ -40,10 +41,10 @@ func main() {
 					hitpoints: STARTING_HITPOINTS,
 				})
 				cave.SetTile(&Tile{
-					kind:'.',
-					x:x,
-					y:y,
-					unit:units[len(units)-1],
+					kind: '.',
+					x:    x,
+					y:    y,
+					unit: units[len(units)-1],
 				})
 			}
 		}
@@ -65,45 +66,80 @@ func main() {
 		//for _,unit := range units {
 		//	unitPrinter(unit)
 		//}
-		
+
 		for i := 0; i < len(units); i++ {
 			currentUnit := units[i]
 			if currentUnit.hitpoints > 0 { //check selected unit is not dead
-				nearestTarget, targetDistance,targets := findNearest(currentUnit, units)
-				if nearestTarget != nil {
-					targetsAvailable = true
-					if targetDistance == 1 { //attack
-						attack(nearestTarget,cave)
-					} else { //move
-						move(currentUnit,cave,targets)
+				targets := getTargets(currentUnit, units)
+				if len(targets) != 0 {
+				targetsAvailable = true
+				nearestTargets, distance := getNearestTargets(currentUnit, targets, cave)
+				if len(nearestTargets) != 0 {
+					if distance <= 1 {
+						sort.Sort(Units(nearestTargets))
+						attack(nearestTargets[0], cave)
+					} else {
+						open := getOpen(currentUnit, nearestTargets, cave)
+						move(currentUnit, cave, open)
 					}
 				}
 			}
+			}
 		}
-		time.Sleep(time.Second/10)
-		c := exec.Command("clear")
+		time.Sleep(time.Second / 10)
+		c := exec.Command("clear") //linux
+		//c := exec.Command("cmd", "/c", "cls")  //windows
 		c.Stdout = os.Stdout
 		c.Run()
-		fmt.Println("Round:",round)
+		fmt.Println("Round:", round)
 		mapPrinter(cave)
 		//for _,unit := range units {
 		//	unitPrinter(unit)
 		//}
 		fmt.Println()
-		
+
 	}
-	sum:= 0
+	sum := 0
 	for _, unit := range units {
-		if unit.hitpoints > 0{
-		sum = sum + unit.hitpoints
+		if unit.hitpoints > 0 {
+			sum = sum + unit.hitpoints
 		}
 	}
-	fmt.Println("Sum:", sum,"Round:", round,"Outcome:",sum*(round))
-	
+	fmt.Println("Sum:", sum, "Round:", round, "Outcome:", sum*(round-1))
+
 }
 
-func move(currentUnit *Unit, cave World, targets []*Unit){
-	open := make([]*Tile,0)
+func move(currentUnit *Unit, cave World, open []*Tile) {
+	//which squares in fewest steps
+	var shortPath []astar.Pather
+	var compare float64 = 1000
+	for i := range open {
+		path, distance, found := astar.Path(cave.Tile(currentUnit.x, currentUnit.y), open[i])
+		if found && distance < compare {
+			shortPath = path
+			compare = distance
+		}
+	}
+	if compare < 1000 {
+		//move one space on shortest path
+		toT := shortPath[len(shortPath)-2].(*Tile)
+
+		cave.Tile(currentUnit.x, currentUnit.y).unit = nil
+		currentUnit.x = toT.x
+		currentUnit.y = toT.y
+		cave.Tile(currentUnit.x, currentUnit.y).unit = currentUnit
+	}
+}
+
+func attack(target *Unit, cave World) {
+	target.hitpoints = target.hitpoints - 3
+	if target.hitpoints <= 0 {
+		cave.Tile(target.x, target.y).unit = nil
+	}
+}
+
+func getOpen(currentUnit *Unit, targets []*Unit, cave World) []*Tile {
+	open := make([]*Tile, 0)
 	for i := range targets {
 		t := targets[i]
 
@@ -114,57 +150,59 @@ func move(currentUnit *Unit, cave World, targets []*Unit){
 			{0, -1},
 			{0, 1},
 		} {
-			if n := cave.Tile(t.x+offset[0],t.y+offset[1]); 
-				n != nil &&
+			if n := cave.Tile(t.x+offset[0], t.y+offset[1]); n != nil &&
 				n.kind != '#' &&
 				n.unit == nil {
 				open = append(open, n)
 			}
 		}
-
 	}
-
-	//which squares in fewest steps
-	var shortPath []astar.Pather
-	var compare float64 = 1000
-	for i := range open {
-		path, distance, found := astar.Path(cave.Tile(currentUnit.x,currentUnit.y),open[i])
-		if found && distance < compare {
-			shortPath = path
-			compare = distance
-		}
-	}
-	if compare < 1000 {
-	//move one space on shortest path
-	toT := shortPath[len(shortPath)-2].(*Tile)
-
-	cave.Tile(currentUnit.x,currentUnit.y).unit = nil
-	currentUnit.x = toT.x
-	currentUnit.y = toT.y
-	cave.Tile(currentUnit.x,currentUnit.y).unit = currentUnit
-	}
+	return open
 }
 
-func attack(target *Unit,cave World){
-	target.hitpoints = target.hitpoints - 3
-	if target.hitpoints <= 0 {
-		cave.Tile(target.x,target.y).unit = nil
+func getTargets(currentUnit *Unit, units []*Unit) []*Unit {
+	targets := make([]*Unit, 0)
+	for j := range units {
+		possibleTarget := units[j]
+		if currentUnit.race != possibleTarget.race && //check if different race
+			possibleTarget.hitpoints > 0 { //check if not dead
+			targets = append(targets, possibleTarget)
+		}
 	}
+	return targets
+}
+
+func getNearestTargets(currentUnit *Unit, targets []*Unit, cave World) ([]*Unit, float64) {
+	var nearestTargets []*Unit = []*Unit{}
+	var shortestDistance float64 = 10000
+	for _, target := range targets {
+		tile := cave.Tile(target.x, target.y)
+		tile.unit = nil
+		_, distance, found := astar.Path(cave.Tile(currentUnit.x, currentUnit.y),tile)
+		tile.unit = target
+		if found && distance < shortestDistance {
+			nearestTargets = []*Unit{target}
+			shortestDistance = distance
+		} else if found && distance == shortestDistance {
+			nearestTargets = append(nearestTargets, target)
+		}
+	}
+	return nearestTargets, shortestDistance
 }
 
 func findNearest(currentUnit *Unit, units []*Unit) (*Unit, int, []*Unit) {
 	//find targets
 	var nearestTarget *Unit
 	targetDistance := int(^uint(0) >> 1)
-	targets := make([]*Unit,0)
+	targets := make([]*Unit, 0)
 	for j := range units {
 		possibleTarget := units[j]
 		if currentUnit.race != possibleTarget.race && //check if different race
 			possibleTarget.hitpoints > 0 { //check if not dead
-			targets = append(targets,possibleTarget)
+			targets = append(targets, possibleTarget)
 			possibleDistance := distanceBetween(currentUnit, possibleTarget)
 			if possibleDistance < targetDistance ||
-			  (possibleDistance == targetDistance && nearestTarget.hitpoints > possibleTarget.hitpoints) {
+				(possibleDistance == targetDistance && nearestTarget.hitpoints > possibleTarget.hitpoints) {
 				nearestTarget = possibleTarget
 				targetDistance = possibleDistance
 			}
@@ -179,46 +217,45 @@ func distanceBetween(unit *Unit, unit1 *Unit) int {
 
 func unitPrinter(unit *Unit) {
 	if unit.hitpoints > 0 {
-	fmt.Print("{")
-	fmt.Print(string(unit.race), " ")
-	fmt.Print("x:", unit.x, " y:", unit.y, " ")
-	fmt.Print(unit.hitpoints, "}")
+		fmt.Print("{")
+		fmt.Print(string(unit.race), " ")
+		//fmt.Print("x:", unit.x, " y:", unit.y, " ")
+		fmt.Print(unit.hitpoints, "}")
 	}
 }
 
 func mapPrinter(cave World) {
 
-	h:= len(cave)
-	w:= len(cave[0])
+	h := len(cave)
+	w := len(cave[0])
 
-	for y:=0; y < h; y++  {
-		units := make([]*Unit,0)
-		for x:=0; x<w; x++ {
-			tile := cave.Tile(x,y)
+	for y := 0; y < h; y++ {
+		units := make([]*Unit, 0)
+		for x := 0; x < w; x++ {
+			tile := cave.Tile(x, y)
 			if tile.unit == nil || tile.unit.hitpoints <= 0 {
 				fmt.Print(string(tile.kind))
 			} else {
 				fmt.Print(string(tile.unit.race))
-				units = append(units,tile.unit)
+				units = append(units, tile.unit)
 			}
 		}
 		fmt.Print("  ")
-		for _,unit := range units {
+		for _, unit := range units {
 			unitPrinter(unit)
 		}
 		fmt.Println()
 	}
 }
 
-
 type World map[int]map[int]*Tile
 
-type Tile struct{
+type Tile struct {
 	kind rune
 	cave World
 	unit *Unit
-	x int
-	y int
+	x    int
+	y    int
 }
 
 type Unit struct {
@@ -252,8 +289,8 @@ func (t *Tile) PathNeighbors() []astar.Pather {
 		{0, -1},
 		{0, 1},
 	} {
-		if n := cave.Tile(t.x+offset[0],t.y+offset[1]); n != nil &&
-			n.kind != '#' && n.unit == nil{
+		if n := cave.Tile(t.x+offset[0], t.y+offset[1]); n != nil &&
+			n.kind != '#' && n.unit == nil {
 			neighbours = append(neighbours, n)
 		}
 	}
@@ -276,8 +313,6 @@ func (t *Tile) PathEstimatedCost(to astar.Pather) float64 {
 	}
 	return float64(absX + absY)
 }
-
-
 
 //Unit slice type and sorting functions for sort.Sort interface
 type Units []*Unit
